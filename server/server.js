3279d61b9501c6430c7e29eccef4ed2f5486582d
Pranav <pranavdhondkar@gmail.com>
2026-03-19 10:24:54 +0530

    feat: setup express server with cors and dotenv

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv"
+import express from "express"
+import cors from "cors"
+
+dotenv.config()
+
+const app=express()
+
+app.use(cors()) // Frontend <-> Backend
+app.use(express.json()) //understand json data
+
+const PORT=process.env.PORT ||5000
+
+app.get("/",(req,res)=>{
+    res.json({message:"YojnaSathi API is running"})
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})

52a00483af9ddc07a5ef886ed6af5698345584e0
Pranav <pranavdhondkar@gmail.com>
2026-03-20 21:07:45 +0530

    feat: add authentication system with JWT

---
+++ server/controllers/authController.js
@@ -0,0 +1,67 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+//generating token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    //encrypting passward
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    //create user in db
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    return res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv"
 import express from "express"
 import cors from "cors"
 import connectDB from "./config/db.js"
+import authRoutes from './routes/authRoutes.js'
+import { protect } from "./middleware/authMiddleware.js"
 
 dotenv.config()
 connectDB()
@@ -10,11 +12,19 @@ const app=express()
 
 app.use(cors()) // Frontend <-> Backend
 app.use(express.json()) //understand json data
+app.use('/api/auth',authRoutes)
 
 const PORT=process.env.PORT ||5000
 
 app.get("/",(req,res)=>{
     res.json({message:"YojnaSathi API is running"})
+})
+
+app.get('/api/protected',protect,(req,res)=>{
+    res.json({
+        message:'You are logged in',
+        user:req.user
+    })
 })
 
 app.listen(PORT,()=>{

7f86931ddc3911fa5ba83ebc96582f5c0433d50b
Pranav <pranavdhondkar@gmail.com>
2026-04-07 10:45:41 +0530

    chore: prepare app for production deployment

--- server/server.js
+++ server/server.js
@@ -1,37 +1,42 @@
-import dotenv from "dotenv"
-import express from "express"
-import cors from "cors"
-import connectDB from "./config/db.js"
-import authRoutes from './routes/authRoutes.js'
-import { protect } from "./middleware/authMiddleware.js"
-import schemeRoutes from "./routes/schemeRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-import adminRoutes from "./routes/adminRoutes.js"
-dotenv.config()
-connectDB()
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import { protect } from "./middleware/authMiddleware.js";
+import schemeRoutes from "./routes/schemeRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+dotenv.config();
+connectDB();
 
-const app=express()
+const app = express();
 
-app.use(cors()) // Frontend <-> Backend
-app.use(express.json()) //understand json data
-app.use('/api/auth',authRoutes)
-app.use('/api/schemes',schemeRoutes)
-app.use('/api/users',userRoutes)
-app.use('/api/admin',adminRoutes)
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+); // Frontend <-> Backend
+app.use(express.json()); //understand json data
+app.use("/api/auth", authRoutes);
+app.use("/api/schemes", schemeRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
 
-const PORT=process.env.PORT ||5000
+const PORT = process.env.PORT || 5000;
 
-app.get("/",(req,res)=>{
-    res.json({message:"YojnaSathi API is running"})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "YojnaSathi API is running" });
+});
 
-app.get('/api/protected',protect,(req,res)=>{
-    res.json({
-        message:'You are logged in',
-        user:req.user
-    })
-})
+app.get("/api/protected", protect, (req, res) => {
+  res.json({
+    message: "You are logged in",
+    user: req.user,
+  });
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

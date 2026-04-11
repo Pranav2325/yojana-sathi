f4a7eee34b565def9f6230e894398bdff78179f3
Pranav <pranavdhondkar@gmail.com>
2026-04-11 21:59:25 +0530

    fix: correct schemeId parameter case in user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -8,6 +8,6 @@ router.get('/profile',protect,getProfile)
 router.put('/profile',protect,updateProfile)
 router.put('/save/:schemeId',protect,saveScheme)
 router.get('/saved',protect,getSavedSchemes)
-router.put('/schemes/:schemeid/status',protect,updateSchemeStatus)
+router.put('/schemes/:schemeId/status',protect,updateSchemeStatus)
 
 export default router
